abd6e64658dfa8599271fd98badc0499cb5e94b3
Slepy29 <maciejslepowronski@gmail.com>
2024-06-28 23:26:07 +0200

    Fixes

--- Revenue Recognition System/Models/Client.cs
+++ Revenue Recognition System/Models/Client.cs
@@ -3,12 +3,12 @@
 public class Client
 {
     public int ClientId { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber{ get; set; }
     public bool IsCompany { get; set; }
-    public string PESEL { get; set; }
-    public string KRS { get; set; }
+    public string? PESEL { get; set; }
+    public string? KRS { get; set; }
     public ICollection<Transaction> Transactions { get; set; }
     public ICollection<Payment> Payments { get; set; }
     public ICollection<Contract> Contracts { get; set; }
